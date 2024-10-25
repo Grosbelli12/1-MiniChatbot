ba36a34b6fc635aa3839768d3bce85ce9e83d1c3
GustavoGrosbelli <gustavogrosbelli26@gmail.com>
2024-10-25 16:11:37 -0300

    ex-feito

--- src/Main.java
+++ src/Main.java
@@ -2,50 +2,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-
-        MiniChatbot miniChatbot = new MiniChatbot();
-        String fala = miniChatbot.dizerOla();
-
-        System.out.println(fala);
-
-        System.out.print("Usuário: ");
-
-        String ajuda = scanner.next();
-
-        String TudoBem = miniChatbot.comoEstou();
-
-        String Nome = miniChatbot.MeuNome();
-
-        String PossoAjudar = miniChatbot.ajudar();
-
-        String erro = miniChatbot.naoEntendiaPergunta();
-
-
-        if (ajuda.contains("oi") || ajuda.contains("Olá")) {
-            System.out.println("MiniChatbot: " + PossoAjudar);
-        } else {
-            System.out.println(erro);
+        Scanner scan = new Scanner(System.in);
+        String resposta = "";
+        MiniChatbot bot = new MiniChatbot();
+        bot.dizerOla();
+        while (!resposta.equals("sair")) {
+            resposta = scan.nextLine();
+            if (resposta.toLowerCase().contains("oi") || resposta.toLowerCase().contains("olá")) {
+                bot.ajudar();
+            } else if (resposta.toLowerCase().contains("como você esta?")) {
+                bot.comoEstou();
+            } else if (resposta.toLowerCase().contains("você gosta de esportes?")) {
+                bot.esportes();
+            } else if (resposta.toLowerCase().contains("qual é o seu nome?")) {
+                bot.MeuNome();
+            } else if (resposta.toLowerCase().contains("sair")) {
+                bot.sairr();
+            } else {
+                bot.naoEntendiaPergunta();
+            }
         }
-
-        System.out.print("Usuário: ");
-        String EstaBem = scanner.next();
-
-        if (EstaBem.contains("Como você está?")) {
-            System.out.println("MiniChatbot: " + TudoBem);
-        } else {
-            System.out.println(erro);
-        }
-
-        System.out.print("Usuário: ");
-        String nome = scanner.next();
-
-        if (nome.contains("Qual é o seu nome?")) {
-            System.out.println("MiniChatbot: " + Nome);
-        } else {
-            System.out.println(erro);
-        }
-
     }
 }
--- src/MiniChatbot.java
+++ src/MiniChatbot.java
@@ -1,30 +1,32 @@
 public class MiniChatbot {
 
 
-    public String dizerOla (){
-        return "Olá! Eu sou o MiniChatbot. Como posso te ajudar hoje? ";
+    public void dizerOla() {
+        System.out.println("Olá! Eu sou o MiniChatbot. Como posso te ajudar hoje? ");
     }
 
-    public String MeuNome(){
-        return "Eu sou o MiniChatbot, seu assistente virtual!";
+    public void ajudar() {
+        System.out.println("Olá! Como posso ajudar?");
+    }
+
+    public void esportes() {
+        System.out.println("Sim, eu gosto por fazerem bem a saúde ");
+    }
+
+    public void MeuNome() {
+        System.out.println("Eu sou o MiniChatbot, seu assistente virtual!");
+    }
+
+    public void comoEstou() {
+        System.out.println(" Estou funcionando perfeitamente, obrigado!!");
+    }
+
+    public void naoEntendiaPergunta() {
+        System.out.println("Desculpe, não entendi sua pergunta. Pode reformular?");
     }
 
 
-    public String naoEntendiaPergunta (){
-        return "Desculpe, não entendi sua pergunta. Pode reformular?";
+    public void sairr() {
+        System.out.println("Tchau! Até a próxima.");
     }
-
-public String comoEstou(){
-        return " Estou funcionando perfeitamente, obrigado!!";
-}
-
-
-public String sairr (){
-        return "Tchau! Até a próxima.";
-}
-
-public String ajudar (){
-        return "Olá! Como posso ajudar?";
-}
-
 }
